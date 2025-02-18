700f50e36d6791ddaf615912b2b532e2dd439e4d
VinayakBhat2699 <bhatvinayak18@gmail.com>
2025-02-18 20:49:25 +0530

    added array

--- js1.js
+++ js1.js
@@ -1,3 +1,6 @@
 let username="vinayak"
 let age=25
-console.log(username)
+console.log("my name is",username,age)
+let  array1=[1,2,3,4,5]
+console.log(typeof(array1))
+console.log("array is",array1)
