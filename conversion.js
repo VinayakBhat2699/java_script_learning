d728a4023cf9325f374cba6b2365ddd98675c850
VinayakBhat2699 <bhatvinayak18@gmail.com>
2025-02-28 20:05:26 +0530

    practiced related to conversion

---
+++ conversion.js
@@ -0,0 +1,14 @@
+//let a= 18
+//console.log(a)
+//console.log(a + " is a"+typeof(a))
+let a=String(18)
+console.log(a + " is a"+typeof(a))
+let b= a+8
+console.log(b)
+console.log(b + " is a"+typeof(b))
+let c=a-5
+console.log(c)
+console.log(c + " is a"+typeof(c))
+let d=Number("2354658")
+console.log(d)
+console.log(d + " is a"+typeof(d))
