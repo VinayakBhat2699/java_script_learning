f8382baff87af7836b24bc8c65d4054f9a3fde5a
VinayakBhat2699 <bhatvinayak18@gmail.com>
2025-02-28 20:08:07 +0530

    tried some examples

---
+++ try.js
@@ -0,0 +1,7 @@
+console.log(typeof 42); // "number"
+console.log(typeof "hello"); // "string"
+console.log(typeof true); // "boolean"
+console.log(typeof undefined); // "undefined"
+console.log(typeof null); // "object" (historical bug in JavaScript)
+console.log(typeof Symbol("id")); // "symbol"
+console.log(typeof BigInt(9007199254740991)); // "bigint"
